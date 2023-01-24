9b3ab5578af5de70de8eaa1730f0ecdb5e10628f
K4L4F0N <kalafon125@gmail.com>
2023-01-24 23:40:42 +0100

    re

--- script.js
+++
@@ -1,8 +0,0 @@
-menu = document.querySelector(".hamburger");
-menu.onclick = function() {
-    navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
-
-    animacija = document.querySelector(".menu");
-    animacija.classList.toggle("active");
-}
